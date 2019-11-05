e5cc1d3dd6b459c91ad0176a7b3b22dc4303d673
chrisgcs <christopher.gilbert@sansano.usm.cl>
2019-11-05 00:03:25 -0300

    Se solicita id destino al enviar el mensaje
    Tambien se agregan en el README los comandos utiles para el desarrollo
    
    se cambia la definicion del idc para ser un atributo de la clase cliente

--- Actividad 1/Client.py
+++ Actividad 1/Client.py
@@ -6,41 +6,44 @@ import mensajeria_pb2_grpc
 
 from datetime import datetime
 
-idc = None
+
 
 class Client:
     mensajes = []
+
     def __init__(self):
         mensajes = []
         self.channel = grpc.insecure_channel('172.18.18.2:5000')
         self.stub = mensajeria_pb2_grpc.SendStub(self.channel)
+        self.idc = None
         threading.Thread(target=self.waitMsg, daemon=True).start()
     
     def HandShake(self):
-        global idc
+        
         stub = mensajeria_pb2_grpc.HandShakeStub(self.channel)
         idReq = mensajeria_pb2.Empty()
-        idc = stub.HandShake(idReq).id
+        self.idc = stub.HandShake(idReq).id
 
     def waitMsg(self):
-        global idc
+        
         while True:
             stub = mensajeria_pb2_grpc.ReceiveStub(self.channel)
-            mesg = mensajeria_pb2.Requester(id_requester = idc)
+            mesg = mensajeria_pb2.Requester(id_requester = self.idc)
             response = stub.Receive(mesg)
             if response.id != 0 :
                 print(response)
 
     def SendMessage(self):
+        destino = input("Ingrese destino: ")
         message = input("Ingrese Mensaje: ")
         if message is not '':
-            mesg = mensajeria_pb2.Mensaje(msg = message, id = 1, id_dest = 2, timestamp = str(datetime.now()) )
+            mesg = mensajeria_pb2.Mensaje(msg = message, id = self.idc, id_dest = int(destino), timestamp = str(datetime.now()) )
             self.stub.Send(mesg)
     
 def Main():
     client = Client()
     client.HandShake()
-    print(idc)
+    print(client.idc)
     while(True):
         client.SendMessage()
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tarea2distribuidos
+
+# Actividad 1
+comandos:
+docker-compose up //levanta arquitectura cliente servidor con 2 clientes
+docker exec -it cliente1 bash //Crear un nuevo cliente que se conecta al servidor
+docker attach [CONTAINER] // Se conecta a la terminal del container especificado (en este caso la de python que esta en ejecucion)
+
+docker-compose run [CONTAINER] //Para crear un nuevo cliente en un nuevo container que se conectara al servidor ya creado
