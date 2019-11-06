707136eca43f53a8ab8c31d743584d91e395d514
jeangonzaleza <jean.gonzaleza@sansano.usm.cl>
2019-11-06 17:33:28 -0300

    ClientsId and HandShake

--- Actividad 2/Client.py
+++ Actividad 2/Client.py
@@ -3,6 +3,32 @@ import threading
 import time
 import sys
 
+id_client = None
+
+class HandShake(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='HandShakeQueue')
+        self.handshake()
+        return
+
+    def callback(self, ch, method, properties, body):
+        global id_client
+        id_client = int(body)
+        print("[*] Received id %r" % id_client)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        self.channel.stop_consuming()
+        self.connection.close()
+
+    def handshake(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue='HandShakeQueue', on_message_callback=self.callback)
+        self.channel.start_consuming()
+
 class Consumer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -10,7 +36,7 @@ class Consumer(threading.Thread):
     def run(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='chat') #Create a queue named chat
+        self.channel.queue_declare(queue='GlobalQueue') #Create a queue named GlobalQueue
         self.receive()
     
     def callback(self, ch, method, properties, body):
@@ -18,7 +44,7 @@ class Consumer(threading.Thread):
 
     def receive(self):
         print(' [*] Waiting for messages. To exit press CTRL+C')
-        self.channel.basic_consume(queue='chat', auto_ack=True, on_message_callback=self.callback) #Recibe mensajes de la cola
+        self.channel.basic_consume(queue='GlobalQueue', auto_ack=True, on_message_callback=self.callback) #Recibe mensajes de la cola
         self.channel.start_consuming()
 
 class Producer(threading.Thread):
@@ -28,20 +54,35 @@ class Producer(threading.Thread):
     def run(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='chat') #Create a queue named chat
+        self.channel.queue_declare(queue='GlobalQueue') #Create a queue named GlobalQueue
+        self.handshake()
+        print("Su id es: ",id_client)
         while(True):
             self.send()
 
     def send(self):
         msg = input("[*] Ingrese mensaje: ")
-        self.channel.basic_publish(exchange='', routing_key='chat', body=msg) #Envia msg a la cola
+        self.channel.basic_publish(exchange='', routing_key='GlobalQueue', body=msg) #Envia msg a la cola
+
+    def handshake(self):
+        self.channel.basic_publish(exchange='', routing_key='GlobalQueue', body="peticion")
 
 def Main():
     try:
-        consumer = Consumer()
+        global id_client
+        #consumer = Consumer()
         producer = Producer()
-        consumer.start()
+        handshake = HandShake()
+        #consumer.start()
         producer.start()
+        handshake.start()
+        while(id_client == None):
+            continue
+        print("Su id es: ", id_client)
+        handshake.join()
+        print("handshake killed")
+
+
     except KeyboardInterrupt:
         print("Bye")
 
--- Actividad 2/Server.py
+++ Actividad 2/Server.py
@@ -1,2 +1,59 @@
+import pika
+import threading
+import time
+import sys
 
+id_actual = 1
 
+class Consumer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')) #Connect to rabbitmq
+        self.channel = self.connection.channel() #Create the channel
+        self.channel.queue_declare(queue='GlobalQueue') #Create a queue named GlobalQueue
+        self.channel.queue_declare(queue='HandShakeQueue') #Create a queue named HandShakeQueue
+        self.receive()
+    
+    def callback(self, ch, method, properties, body):
+        global id_actual
+        msg = body
+        print("[*] Received %r" % msg)
+        if (msg == b'peticion'):
+            self.channel.basic_publish(exchange='', routing_key='HandShakeQueue', body=str(id_actual))
+            id_actual += 1
+            print ("siguiente id: ",id_actual)
+
+    def receive(self):
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.basic_consume(queue='GlobalQueue', auto_ack=True, on_message_callback=self.callback) #Recibe mensajes de la cola
+        self.channel.start_consuming()
+
+class Producer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='GlobalQueue')
+        self.channel.queue_declare(queue='HandShakeQueue')
+        while(True):
+            self.send()
+
+    def send(self):
+        msg = input("[*] Ingrese mensaje: ")
+        self.channel.basic_publish(exchange='', routing_key='GlobalQueue', body=msg) #Envia msg a la cola
+
+def Main():
+    try:
+        consumer = Consumer()
+        #producer = Producer()
+        consumer.start()
+        #producer.start()
+    except KeyboardInterrupt:
+        print("Bye")
+
+if __name__ == '__main__':
+    Main()
