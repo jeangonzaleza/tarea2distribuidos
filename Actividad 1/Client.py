71d0767565ff03237ce6cec6f8a71a5cb567937f
chrisgcs <christopher.gilbert@sansano.usm.cl>
2019-11-06 01:35:24 -0300

    Servicio de menu
    Se crea Servicio Menu para que cliente pueda seleccionar que opcion desea usar. Falta probar

--- Actividad 1/Client.py
+++ Actividad 1/Client.py
@@ -39,12 +39,19 @@ class Client:
         if message is not '':
             mesg = mensajeria_pb2.Mensaje(msg = message, id = self.idc, id_dest = int(destino), timestamp = str(datetime.now()) )
             self.stub.Send(mesg)
+        
+    def Select_Menu(self):
+        opcion = input("Seleccione una de las siguiente opciones: \n \t 1-Lista usuarios \n \t 2-Lista mensajes enviados \n \t 3-Enviar mensaje")
+        mesg = mensajeria_pb2.Mensaje(msg = opcion, id = self.idc, id_dest = 0, timestamp = str(datetime.now()) )
+        response = self.stub.Menu(mesg)
+        print response.lista
     
 def Main():
     client = Client()
     client.HandShake()
     print(client.idc)
     while(True):
+        client.Select_Menu()
         client.SendMessage()
 
 if __name__=='__main__':
